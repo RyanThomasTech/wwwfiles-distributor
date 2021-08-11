957b327416816a42dd0449f642e25a9c5b178ac9
Ryan Thomas <contact@ryanthomas.tech>
2021-08-10 23:52:15 -0500

    Added core functionality for vps implementation

--- update_site_files.sh
+++ update_site_files.sh
@@ -5,10 +5,10 @@ if [ "$#" -ne 1 ]; then
 fi
 
 if [ "$1" = "vps" ]; then
-    LAST_SHA_FILE=/home/ryan/cuddly_broccoli_sha
+    LAST_SHA_FILE=/var/lib/cuddly_broccoli_sha
 else
     echo "unrecognized argument"
-    return 1
+    exit 1
 fi
 
 
@@ -21,10 +21,18 @@ fi
 LAST_SHA=$( cat $LAST_SHA_FILE )
 
 if [ $LAST_SHA = $HEAD_SHA ]; then
-    echo "they match"
-    return 0
+    exit 0
 else
-    echo "no match here"
     echo $HEAD_SHA | tee $LAST_SHA_FILE >/dev/null
+#TODO: consider separating removal of old files to be dependent upon success of getting new ones
+    if [ -d /etc/nginx/sites-available/cuddly-broccoli ]; then
+        rm -r /etc/nginx/sites-available/cuddly-broccoli
+    fi
+    git clone https://github.com/RyanThomasTech/cuddly-broccoli /tmp/sitefiles/cuddly-broccoli
+    mv /tmp/sitefiles/cuddly-broccoli/$1 /etc/nginx/sites-available/cuddly-broccoli
+    rm -r /tmp/sitefiles/cuddly-broccoli
+    if [ ! -f /etc/nginx/sites-enabled/cuddly-broccoli ]; then
+        ln -s /etc/nginx/sites-available/cuddly-broccoli /etc/nginx/sites-enabled/cuddly-broccoli
+    fi
 fi
-
+exit 0
